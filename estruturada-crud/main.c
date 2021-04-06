349edde8e565a2b7e7decaa0d9f680689a0f6910
Luiz Carlos da Silva Araujo <luizcarlos_bsb2006@hotmail.com>
2021-04-06 12:13:28 -0300

    Initial Commit

---
+++ estruturada-crud/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  estruturada-crud
+//
+//  Created by Luiz Carlos da Silva Araujo on 06/04/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
