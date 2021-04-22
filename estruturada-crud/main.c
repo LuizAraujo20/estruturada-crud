edf8250839e37fbb31e3fd29303445c5b038e6e4
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-04-22 13:26:22 -0300

    :gift: Creat with auto-increment

--- estruturada-crud/main.c
+++ estruturada-crud/main.c
@@ -8,15 +8,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+//DECLARAÇÃO DE TIPOS
 struct tProdutos{
     int codigo;
     char nome[20];
     float preco;
 };
 
+//PROTÓTIPOS
+void mostrarProduto(struct tProdutos prod);
+void incluirProduto();
+
 int main(int argc, const char * argv[]) {
 //declaracoes
-    FILE *arq;
     struct tProdutos produto;
 //    int indice;
     char opcao = 'a';
@@ -43,33 +47,12 @@ int main(int argc, const char * argv[]) {
             //incluir
                 
                 
-                modularizar
-                
-                
                 
                 printf("\n\n\n");
                 printf("--> INCLUIR PRODUTO");
                 
-                receberProduto(&produto, );
-                printf("\nCODIGO: ");
-                scanf("%d", &produto.codigo);
-                printf("NOME  : ");
-                fflush(stdin);
-                gets(produto.nome);
-                printf("PRECO : ");
-                scanf("%f", &produto.preco);
+                incluirProduto();
                 
-                arq = fopen("db_produtos.dat", "ab");
-                
-                if(arq == NULL){
-                    printf("\n\n");
-                    printf("ERRO AO ABRIR O ARQUIVO!");
-                    fclose(arq);
-                    printf("\n\n");
-                } else {
-                    fwrite(&produto, sizeof(produto), 1, arq);
-                    fclose(arq);
-                }
                 
                 break;
                 
@@ -79,7 +62,7 @@ int main(int argc, const char * argv[]) {
                 printf("--> LISTAR TUDO \n");
                 printf("\n%-6s %-20s %-12s", "CODIGO", "NOME", "PRECO(R$)");
                 
-                arq = fopen("db_produtos.dat","rb");
+                FILE *arq = fopen("bd_produtos.dat","rb");
                 
                 while(fread(&produto, sizeof(produto),1,arq)){
                     printf("\n%06d %-20s %9.2f", produto.codigo, produto.nome, produto.preco);
@@ -108,8 +91,51 @@ int main(int argc, const char * argv[]) {
             case '0':
             //sair precisa??????????
                 printf("\n\n\n");
+                return 0;
                 break;
         
         }//fim switch: opcoes menu principal
-    }while(opcao != '0');//do usuario menu principal
+    }while(1);//do usuario menu principal
 }
+
+
+
+void mostrarProduto(struct tProdutos prod){
+    printf("\n\n%-6s %-20s %-12s", "CODIGO", "NOME", "PRECO(R$)");
+    printf("\n%06d %-20s %9.2f", prod.codigo, prod.nome, prod.preco);
+}
+
+void incluirProduto(){
+    struct tProdutos prod;
+    int ultimoCodigo = 0;
+    char opcao = 's';
+    
+    //AUTOINCREMENT COD
+    FILE *arq;
+    arq = fopen("bd_produtos.dat", "rb");
+
+    while(fread(&prod, sizeof(prod), 1, arq)){
+        ultimoCodigo = prod.codigo + 1;
+        printf("\n\n\n\nultimoCodigo+ 1\n\n\n");
+//        ultimoCodigo++;
+    }
+    fclose(arq);//tive que separar read do append, pois nao "read" mesmo com a+b
+    arq = fopen("bd_produtos.dat", "ab");
+    
+    printf("\nCODIGO: %d", ultimoCodigo);
+    prod.codigo = ultimoCodigo;
+    printf("\nNOME: ");
+    fflush(stdin);
+    gets(prod.nome);
+    printf("PRECO : ");
+    scanf("%f", &prod.preco);
+    
+    //SALVAR NO ARQUIVO
+    if(arq != NULL){
+        fwrite(&prod, sizeof(prod), 1, arq);
+    }
+    
+    fclose(arq);
+}
+
+
